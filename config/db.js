8e981c0ac849445aee0d422f5933c09a6856b881
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2023-06-19 12:27:26 -0600

    mongoose

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-import mongooose from "mongoose";
+import mongoose from "mongoose";
 
 const conectarDB = async () => {
   try {
-    const db = await mongooose.connect(
+    const db = await mongoose.connect(
         process.env.MONGO_URI,
       {
         useNewUrlParser: true,
